c54633739d1231f8b41c15c86828084253c38f83
Jenary Madia <jenary.madia@rebisco.com.ph>
2024-08-14 21:30:41 +0800

    add edit function on packages

--- app/Livewire/Packages/Edit.php
+++ app/Livewire/Packages/Edit.php
@@ -2,12 +2,59 @@
 
 namespace App\Livewire\Packages;
 
+use App\Models\Package;
+use Illuminate\Support\Facades\Auth;
+use Jantinnerezo\LivewireAlert\LivewireAlert;
 use Livewire\Component;
 
 class Edit extends Component
 {
+    use LivewireAlert;
+
+    public $name;
+    public $description;
+    public $price;
+    public $package;
+
+    protected function rules()
+    { 
+        return [
+            'name' => 'required|string|max:255|unique:packages,name,' . $this->package->id,
+            'description' => 'nullable|string',
+            'price' => 'required|numeric|min:0',
+        ];
+    }
+
+    public function mount(Package $package)
+    {
+        $this->package = $package;
+        $this->name = $package->name;
+        $this->description = $package->description;
+        $this->price = $package->price;
+    }
+
+    public function submit()
+    {
+        $validatedData = $this->validate();
+
+        $this->package->update([
+            'name' => $this->name,
+            'description' => $this->description,
+            'price' => $this->price,
+        ]);
+
+        $this->alert('success', 'Package successfully updated', [
+            'toast' => false,
+            'position' => 'center'
+        ]);
+
+        // Optionally, you can reset the form fields or redirect the user
+        $this->reset(['name', 'description', 'price']);
+        return redirect()->route('packages.index'); // Redirect to the package list page
+    }
+
     public function render()
     {
-        return view('livewire.packages.edit');
+        return view('livewire.packages.create');
     }
 }
--- resources/views/livewire/packages/edit.blade.php
+++ resources/views/livewire/packages/edit.blade.php
@@ -1,3 +1,89 @@
-<div>
-    {{-- Close your eyes. Count to one. That is how long forever feels. --}}
+<x-slot name="header">
+    <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        {{ __('Edit Package') }}
+    </h2>
+</x-slot>
+
+<div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="p-20 bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+        <!--
+        This example requires some changes to your config:
+        
+        ```
+        // tailwind.config.js
+        module.exports = {
+            // ...
+            plugins: [
+            // ...
+            require('@tailwindcss/forms'),
+            ],
+        }
+        ```
+        -->
+        <form wire:submit.prevent="submit">
+            <div class="space-y-12 sm:space-y-16">
+                <div>
+                <h2 class="text-base font-semibold leading-7 text-gray-900">Package</h2>
+                <p class="mt-1 max-w-2xl text-sm leading-6 text-gray-600">This information will be displayed publicly so be careful what you share.</p>
+
+                <div class="mt-10 space-y-8 border-b border-gray-900/10 pb-12 sm:space-y-0 sm:divide-y sm:divide-gray-900/10 sm:border-t sm:pb-0">
+                    <div class="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
+                    <label for="name" class="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5">Package Name</label>
+                    <div class="mt-2 sm:col-span-2 sm:mt-0">
+                        <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                            <input wire:model="name" type="text" name="name" id="name" autocomplete="name" class="block flex-1 border-0 bg-transparent py-1.5 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="janesmith">
+                        </div>
+                    </div>
+                    </div>
+
+                    <div class="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
+                    <label for="price" class="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5">Price</label>
+                    <div class="mt-2 sm:col-span-2 sm:mt-0">
+                        <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                            <input wire:model="price" type="number" name="price" id="price" autocomplete="price" class="block flex-1 border-0 bg-transparent py-1.5 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="janesmith">
+                        </div>
+                    </div>
+                    </div>
+
+                    <div class="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
+                    <label for="about" class="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5">Description</label>
+                    <div class="mt-2 sm:col-span-2 sm:mt-0">
+                        <textarea wire:model="description" id="about" name="about" rows="3" class="block w-full max-w-2xl rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
+                        <p class="mt-3 text-sm leading-6 text-gray-600">Write a few sentences about yourself.</p>
+                    </div>
+                    </div>
+
+                    <div class="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
+                    <label for="cover-photo" class="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5">Cover photo</label>
+                    <div class="mt-2 sm:col-span-2 sm:mt-0">
+                        <div class="flex max-w-2xl justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+                        <div class="text-center">
+                            <svg class="mx-auto h-12 w-12 text-gray-300" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+                            <path fill-rule="evenodd" d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z" clip-rule="evenodd" />
+                            </svg>
+                            <div class="mt-4 flex text-sm leading-6 text-gray-600">
+                            <label for="file-upload" class="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500">
+                                <span>Upload a file</span>
+                                <input id="file-upload" name="file-upload" type="file" class="sr-only">
+                            </label>
+                            <p class="pl-1">or drag and drop</p>
+                            </div>
+                            <p class="text-xs leading-5 text-gray-600">PNG, JPG, GIF up to 10MB</p>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+
+            <div class="mt-6 flex items-center justify-end gap-x-6">
+                <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
+                <button type="submit" class="inline-flex justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+            </div>
+        </form>
+
+        </div>
+    </div>
 </div>
